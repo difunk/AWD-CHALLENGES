49cd8953679d97a1df7a5dd4ca1e1f3ab839df2f
Dirk <so0mewhat2@gmail.com>
2025-06-25 15:03:41 +0200

    refactor: extract logic of challenge 1 to dedicated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
-import { useEffect, useState } from 'react'
-import { users } from './InputData';
 import './App.css'
+import Challenge1 from './components/Challenge1'
 
 function App() {
-  const [userResults, setUserResults] = useState<Record<string, string[]>>({});
-
-  useEffect(() => {
-    const results: Record<string, string[]> = { online: [], offline: [], away: [] };
-
-    users.forEach((user) => {
-      user.status === "offline"
-        ? results.offline.push(user.username)
-        : user.lastActivity > 10
-        ? results.away.push(user.username)
-        : results.online.push(user.username)
-    });
-
-    Object.keys(results).forEach((key) => {
-      if (results[key].length === 0) {
-        delete results[key];
-      }
-    });
-
-    setUserResults(results);
-  }, []);
-
   return (
     <>
-    <p>
-        Challenge 1:
-    </p>
-      {Object.entries(userResults).map(([status, users]) => (
-        <div key={status}>
-            <strong>{status}:</strong>
-                {users.map(user => (
-                  <div key={user}>{user}</div>
-                ))}
-        </div>
-      ))}
+      <Challenge1 />
     </>
   )
 }
