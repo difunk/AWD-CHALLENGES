2a742d7c4246e6659b3e3fdef577db1101041f04
Dirk <so0mewhat2@gmail.com>
2025-07-03 12:21:24 +0200

    refactor: paldindrom to be unicode save

--- src/utils/palindrome.ts
+++ src/utils/palindrome.ts
@@ -1,3 +1,11 @@
 export function isPalindrome(word: string): boolean {
-  return word === word.split('').reverse().join('');
+  // alternativ: return word === word.split('').reverse().join('');
+  const length = word.length;
+  for (let i = 0; i < length / 2; i++){
+    if (word[i] !== word[length - 1 - i]){
+       return false;
+    }
+  }
+
+  return true;
 }
